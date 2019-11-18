57dc7afe41b9a7589da6a5a7d2c5393653362bc3
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-18 13:13:22 -0300

    Logout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { useSelector } from 'react-redux';
 import createRouter from './routes';
 
 export default function App() {
+  /* se tiver alguma alteração em signed... */
   const signed = useSelector(state => state.auth.signed);
 
+  /* ...recria as rotas */
   const Routes = createRouter(signed);
 
   return <Routes />;
