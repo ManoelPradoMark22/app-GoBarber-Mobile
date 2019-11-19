f3b4b20822f8ebd7c1592b1a50a8ccdef0d0333a
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-19 13:24:57 -0300

    listingProviders

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 /* createSwitchNavigator não permite arrastar a tela de um lado para o outro para
 mudar navegar entre elas, apenas a partir de um botao (é o que queremos
@@ -5,8 +6,21 @@ para as telas de login e cadastro!) */
 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import { createStackNavigator } from 'react-navigation-stack';
+/* createStackNavigator é como uma pilha, enquanto o usuario navega entre
+SelectProvider, SelectDateTime e Confirm, ele vai poder voltar entre essas telas
+caso tenha cometido algum erro etc.
+além disso ele tem memoria, caso queira voltar para uma pagina anterior e corrigir
+um erro. Tem tb a seta acima para voltar */
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -25,6 +39,36 @@ export default (signedIn = false) =>
           {
             /* CONJUNTO DE ROTAS P QND O USER 'ESTIVER' LOGADO NO APP */
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                    headerLayoutPreset: 'center',
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
