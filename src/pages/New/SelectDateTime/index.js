19bb687afd0b75511f42b8e1584eed86e8307fc6
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-21 11:50:23 -0300

    selectingTime

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,20 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   /* vamos passar para o DateInput, vamos usar la */
   const [date, setDate] = useState(new Date());
+
+  /* esse estado é para o horario */
+  const [hours, setHours] = useState([]);
+  /* buscando o parametro provider que veio de dentro da navegacao */
+  const provider = navigation.getParam('provider');
+
+  /* toda vez que o date alterar (quando o usuario selecionar uma data diferente)
+  o useEffect vai ser executado novamente */
+  useEffect(() => {
+    async function loadAvailable() {
+      /* vai pegar os horarios do prestador na data atual com uma prop available
+      (que sera true para hor disponivel e false para hor nao disponivel) */
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          /* passando a data atual em timestamp */
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  /* ta recebendo o value (estamos chamando aqui de time) */
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      /* passando os dados a seguir como parâmetros */
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          /* vamos usar o time como chave pq ja está em string e nao se repete,
+          08:00, 09:00, 10:00 ....... */
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              /* value é a data no estile 2019-11-21T17:00:00-03:00 que recebemosda API */
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
--- src/pages/New/SelectDateTime/styles.js
+++ src/pages/New/SelectDateTime/styles.js
@@ -1,5 +1,34 @@
 import styled from 'styled-components';
+import { RectButton } from 'react-native-gesture-handler';
+/* RectButton além de ter o efeito de clique do Android e do IOS, também aceita
+a propriedade enabled como true ou false, e se a mesma estiver como false, o
+RectButton não vai permitir o clique, coisa que o TouchableOpacity ñ faz */
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const HourList = styled.FlatList.attrs({
+  numColumns: 2,
+  showsVerticalScrollIndicator: false,
+})`
+  padding: 0 20px;
+`;
+
+/* export const Hour = styled.TouchableOpacity``; */
+export const Hour = styled(RectButton)`
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  flex: 1;
+  opacity: ${props => (props.enabled ? 1 : 0.6)};
+
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
 `;
